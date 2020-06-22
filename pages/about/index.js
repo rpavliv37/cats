ccf2630a9dd1916ed0a23e84029409c60eaf1ab7
Roman Pavliv <rpavliv37@gmail.com>
2020-06-22 19:08:54 +0300

    upd

---
+++ pages/about/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+import { Component } from 'react'
+import { attributes, react as HomeContent } from '../content/home.md';
+
+export default class Home extends Component {
+  render() {
+    let { title, cats } = attributes;
+    return (
+      <>
+        <Head>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
+        <article>
+          about page
+        </article>
+      </>
+    )
+  }
+}
